2ed74d4af5a018fd93b61f658b5afa6f5101a55a
Mohit Rane <mrrane99@gmail.com>
2020-10-11 09:59:21 +0530

    Basic File Structure Created

---
+++ src/screens/StartScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const StartScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default StartScreen
---
+++ src/stacks/MainTabStack.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const MainTabStack = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default MainTabStack
